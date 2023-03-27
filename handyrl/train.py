ab6c205edf264e175503df44199160163e0feff9
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2023-03-27 16:21:27 +0900

    feature: progress based entropy reguratition coef exponential decay

--- handyrl/train.py
+++ handyrl/train.py
@@ -210,7 +210,7 @@ def compose_losses(outputs, log_selected_policies, total_advantages, targets, ba
     losses['ent'] = entropy.sum()
 
     base_loss = losses['p'] + losses.get('v', 0) + losses.get('r', 0)
-    entropy_loss = entropy.mul(1 - batch['progress'] * (1 - args['entropy_regularization_decay'])).sum() * -args['entropy_regularization']
+    entropy_loss = entropy.mul(args['entropy_regularization_decay'] ** batch['progress']).sum() * -args['entropy_regularization']
     losses['total'] = base_loss + entropy_loss
 
     return losses, dcnt
